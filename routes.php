e48e81eafd74fc56dac31252ef5d72790f7c8ced
Scott Bedard <scottbedard90@gmail.com>
2016-12-30 08:18:06 -0500

    Reference full path to csrf middleware

--- routes.php
+++ routes.php
@@ -1,5 +1,5 @@
 <?php
 
-Route::group(['middleware' => 'VerifyCsrfToken'], function() {
+Route::group(['middleware' => '\Illuminate\Foundation\Http\Middleware\VerifyCsrfToken'], function() {
     Route::resource('api/bedard/cube/solves', 'Bedard\Cube\Api\Solves');
 });
